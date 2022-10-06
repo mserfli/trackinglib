b3d0454cfa9bc033fba315713171d2a273a35e45
Matthias Serfling <matthias@jarvis.local>
2022-10-06 18:58:59 +0200

    new idea about block diagonal matrix

--- include/trackingLib/math/linalg/block_diagonal_matrix.h
+++ include/trackingLib/math/linalg/block_diagonal_matrix.h
@@ -1,5 +1,7 @@
 #ifndef DD0495D0_48ED_4918_A713_7BBCA6459178
 #define DD0495D0_48ED_4918_A713_7BBCA6459178
 
+// TODO(matthias): implement a matrix class which allows a block structure with zero blocks - shall be able to handle also
+// covariance matrix types
 
 #endif // DD0495D0_48ED_4918_A713_7BBCA6459178
